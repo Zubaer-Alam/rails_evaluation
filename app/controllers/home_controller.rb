86466f7fdb49d6ab7b39c582f45fc0a5e7a56428
Zubaer-Alam <xuvaer@gmail.com>
2023-12-22 17:29:42 +0600

    devise auth working

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController <ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:homepage]
+    def homepage
+    end
+
+    def dashboard
+    end
+end
---
+++ db/migrate/20231220020547_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.integer :quantity
+      t.references :category, null: false, foreign_key: true
+      t.references :brand, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
