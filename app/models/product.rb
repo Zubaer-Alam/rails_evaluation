972e5e34eb07514ec06d70cba0b7abe1cdf6b51a
Zubaer-Alam <xuvaer@gmail.com>
2023-12-29 04:57:02 +0600

    Fix product validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   broadcasts_to ->(_product) { 'products' }, inserts_by: :prepend
