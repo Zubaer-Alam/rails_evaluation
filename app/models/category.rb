a445ed45a86a6f31472fc32cf7476627d45ffb22
Zubaer-Alam <xuvaer@gmail.com>
2024-01-11 01:09:41 +0600

    Abandoned testing. Doing API key

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+
   validates :name, presence: true, uniqueness: { case_sensitive: false },length:{maximum: 25}
+  
   scope :ordered, -> { order(id: :desc) }
   broadcasts_to ->(_category) { 'categories' }, inserts_by: :prepend
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,19 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  scope :ordered, -> {order(id: desc)}
   broadcasts_to ->(_product) { 'products' }, inserts_by: :prepend
+
+  before_validation :normalize_name
+  
+  private
+
+  def normalize_name
+    self.name = name.split.map(&:capitalize).join(' ') if name.present?
+  end
 end
